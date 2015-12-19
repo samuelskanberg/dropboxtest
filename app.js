1d1bd8fc7692e0f5fd0a7da55abbfef342595474
Samuel Skanberg <samuel.skanberg@gmail.com>
2015-12-19 20:49:28 +0100

    Now it logs out as well

--- app.js
+++ app.js
@@ -3,108 +3,22 @@ $(document).ready(function(){
 
   var client = new Dropbox.Client({ key: "xatg1pwrwutb0xc" });
 
-  var hash = window.location.hash.substring(1);
-  var arguments = hash.split("&");
-  var argumentDictionary = {};
-
-  arguments.forEach(function(element, index, array) {
-    var keyValueArray = element.split("=");
-    argumentDictionary[keyValueArray[0]] = keyValueArray[1];
+  client.authenticate({interactive: false}, function(error, client) {
+    if (error) {
+      console.log("Error occured");
+      console.log(error);
+      return ;
+    }
   });
 
-  console.log("dict");
-  console.log(argumentDictionary);
-
-  if (argumentDictionary["access_token"]) {
-    console.log("Came back from a redirect!");
-    window.location.hash = "";
-
-    var client = new Dropbox.Client({
-      key: "xatg1pwrwutb0xc",
-      token: argumentDictionary["access_token"],
-      uid: argumentDictionary["uid"]
-    });
-
-    client.authenticate(function(error, client) {
-      if (error) {
-        console.log("Error with second authenticate");
-        console.log(error);
-        return false;
-      }
-
-      console.log("Logged in");
-
-      $('#logout').click(function() {
-        console.log("Trying to log out");
-        client.signOut(null, function(error) {
-          if (error) {
-            console.log("Logout failed:");
-            console.log(error);
-            return ;
-          }
-          client.reset();
-          console.log("Totally loged out");
-          $('#login_page').show();
-          $('#logged_in_page').hide();
-        });
-
-        return false;
-      });
-
-      $('#list_files').click(function() {
-        console.log("Readdir");
-        client.readdir("/", {removed: false}, function(error, list, filestat, filestats) {
-          if (error) {
-            console.log("Error reading dir");
-            console.log(error);
-            return ;
-          }
-          $('#file_list').empty();
-          list.forEach(function(item) {
-            $('#file_list').append("<li>"+item+"</li>");
-          });
-        });
-      });
-
-      $("#create_file").click(function() {
-        var filename = $('#filename').val();
-        console.log("Creating file: "+filename);
-        if (filename.length == 0) {
-          console.log("Filename can't be empty");
-          return ;
-        }
-
-        client.stat(filename, {deleted: false}, function(error, filestat, filestats) {
-          // Should produce an error since we don't want it to be found
-          if (!error) {
-            console.log("File exists")
-            return;
-          }
-          client.writeFile(filename, "test", {noOverwrite: true}, function(error, stat) {
-            if (error) {
-              console.log("Error occured when creating file");
-              console.log(error);
-              return ;
-            }
-            console.log("File successfully created");
-          });
-        });
-      });
-
-      var credentials = client.credentials();
-      console.log(credentials);
-      $('#credentials').text("User id: "+credentials.uid);
-
-      // Show it
-      $('#login_page').hide();
-      $('#logged_in_page').show();
-
-      return false;
-    });
-
-
+  if (client.isAuthenticated()) {
+    console.log("Authenticated with cached");
+    $('#login_page').hide();
+    $('#logged_in_page').show();
   } else {
-    console.log("No access token found in query params");
+    console.log("Not authenticated with cached");
+    $('#login_page').show();
+    $('#logged_in_page').hide();
   }
 
   $('#login').click(function() {
@@ -128,4 +42,60 @@ $(document).ready(function(){
     });
   });
 
+  $('#logout').click(function() {
+    console.log("Trying to log out");
+    client.signOut(null, function(error) {
+      if (error) {
+        console.log("Logout failed:");
+        console.log(error);
+        return ;
+      }
+      client.reset();
+      console.log("Totally loged out");
+      $('#login_page').show();
+      $('#logged_in_page').hide();
+    });
+
+    return false;
+  });
+
+  $('#list_files').click(function() {
+    console.log("Readdir");
+    client.readdir("/", {removed: false}, function(error, list, filestat, filestats) {
+      if (error) {
+        console.log("Error reading dir");
+        console.log(error);
+        return ;
+      }
+      $('#file_list').empty();
+      list.forEach(function(item) {
+        $('#file_list').append("<li>"+item+"</li>");
+      });
+    });
+  });
+
+  $("#create_file").click(function() {
+    var filename = $('#filename').val();
+    console.log("Creating file: "+filename);
+    if (filename.length == 0) {
+      console.log("Filename can't be empty");
+      return ;
+    }
+
+    client.stat(filename, {deleted: false}, function(error, filestat, filestats) {
+      // Should produce an error since we don't want it to be found
+      if (!error) {
+        console.log("File exists")
+        return;
+      }
+      client.writeFile(filename, "test", {noOverwrite: true}, function(error, stat) {
+        if (error) {
+          console.log("Error occured when creating file");
+          console.log(error);
+          return ;
+        }
+        console.log("File successfully created");
+      });
+    });
+  });
 });
